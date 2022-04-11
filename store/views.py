54ebcdec58e406f6be5333a09e2ea95eab2e04ed
Ming <s3843573@student.rmit.edu.au>
2022-04-11 22:37:51 +1000

    add routes for store

---
+++ store/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'store/home.html')
+
+
+def about(request):
+    return render(request, 'store/about.html')
+
+
+def cart(request):
+    return render(request, 'store/cart.html')
+
+
+def product(request):
+    return render(request, 'store/product.html')
