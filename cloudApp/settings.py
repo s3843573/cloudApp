b5760eb82036b7956cfc9fd418ba20d5f78aad15
Ming <s3843573@student.rmit.edu.au>
2022-04-14 23:34:32 +1000

    change eb configs

--- cloudApp/settings.py
+++ cloudApp/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-6h#=i3&0ze)7m46(*0u^$j@4$fs0q-kgh@(6v_@0i#_x+g8mq0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://assignment1-env.eba-yvmjbxb8.us-east-1.elasticbeanstalk.com/', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'a1-env.eba-wpv8pbvt.us-east-1.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
