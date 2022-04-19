ed6f180d76fe889b1d70ec6e6b87f1ff0f5b4cd3
Ming <s3843573@student.rmit.edu.au>
2022-04-19 21:19:23 +1000

    change media folder settings

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from pickle import TRUE
 from pyexpat import model
 from django.db import models
@@ -10,7 +11,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     price = models.FloatField(null=False)
     category = models.CharField(max_length=100, null=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True,
+                              upload_to="productImages/")
     description = models.CharField(max_length=200, null=True)
 
     def __str__(self):
