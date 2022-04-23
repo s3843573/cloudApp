875e58ac751efe31c36c03f33f7475b08513a2af
Ming <s3843573@student.rmit.edu.au>
2022-04-23 22:41:12 +1000

    clean up and static file settings

--- cloudApp/production.py
+++ cloudApp/production.py
@@ -153,7 +153,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 # Default primary key field type
